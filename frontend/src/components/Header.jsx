d9910ec95dc0fda9b544401d8fe4deac7c8c0925
om singh <iamomsingh11@gmail.com>
2023-11-15 20:19:38 +0530

    routing setup done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import {Navbar, Nav, Container, NavbarBrand, NavbarCollapse, NavbarToggle, NavLink} from 'react-bootstrap';
 import {FaShoppingCart, FaUser} from 'react-icons/fa';
+import {LinkContainer} from 'react-router-bootstrap';
 import logo from '../assets/logo.png'
 
 const Headers = () => {
@@ -7,22 +8,28 @@ const Headers = () => {
    <header>
     <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
         <Container>
+            <LinkContainer to='/'>
             <NavbarBrand href='/'>
                 <img src={logo} alt='omShop' />
                 OmShop
             </NavbarBrand>
+            </LinkContainer>
             <NavbarToggle aria-controls='basic-navbar-nav'></NavbarToggle>
             <NavbarCollapse id='basic-navbar-nav'>
                 <Nav className='ms-auto'>
-                    <NavLink href='/cart'>
+                    <LinkContainer to='/cart'>
+                    <NavLink>
                         <FaShoppingCart />
                         Cart
                     </NavLink>
+                    </LinkContainer>
 
+                    <LinkContainer to='/login'>
                     <NavLink href='/login'>
                         <FaUser />
                         Sign In
                     </NavLink>
+                    </LinkContainer>
                 </Nav> 
             </NavbarCollapse>
         </Container>
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Card, CardBody, CardImg, CardText, CardTitle } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-// import Rating from './Rating';
 
 const Product = ({product}) => {
   return (
     <Card className='my-3 p-3 rounded'>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <CardImg src={product.image} variant='top' />
-        </a>
+        </Link>
 
       <CardBody>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <CardTitle as='div' className='product-title'>
             <strong>{product.name}</strong>
           </CardTitle>
-        </a>
+        </Link>
 
         {/* <CardText as='div'>
           <Rating
